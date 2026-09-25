d84707986b69b876615f51f5a6c44795fd1062db
Pavel Urusov <plu-github@protonmail.com>
2026-09-25 22:52:38 +0100

    add tests for the scanner

---
+++ cmd/go-wayland-scanner/scanner_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+)
+
+func TestGeneratedServerObjectAndFDType(t *testing.T) {
+	prefix = "wl"
+	defer func() { prefix = "" }()
+	protocol = Protocol{Name: "wayland", Interfaces: []Interface{{Name: "wl_data_offer"}}}
+	var output bytes.Buffer
+	writeEventDispatcher(&output, "DataDevice", Interface{Events: []Event{{Name: "data_offer", Args: []Arg{{Name: "id", Type: "new_id", Interface: "wl_data_offer"}}}}})
+	s := output.String()
+	for _, want := range []string{"e.Id = new(DataOffer)", "RegisterServer(e.Id", "if i.dataOfferHandler != nil"} {
+		if !strings.Contains(s, want) {
+			t.Fatalf("missing %q in %s", want, s)
+		}
+	}
+	if strings.Contains(s, "if i.dataOfferHandler == nil") {
+		t.Fatal("unhandled new_id was dropped")
+	}
+	output.Reset()
+	writeEventDispatcher(&output, "DataSource", Interface{Events: []Event{{Name: "target"}, {Name: "send", Args: []Arg{{Name: "fd", Type: "fd"}}}}})
+	if !strings.Contains(output.String(), "TakesFD") || !strings.Contains(output.String(), "case 1: return true") {
+		t.Fatalf("FD event type missing: %s", output.String())
+	}
+}
